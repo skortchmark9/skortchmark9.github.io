0fca248e8d1cae8f5a83f3cb96c71ea86166227b
Sam Kortchmar <Samuel_Kortchmar@brown.edu>
2016-01-19 17:59:11 -0500

    added 404?

---
+++ phonebook/js/main.js
@@ -0,0 +1,153 @@
+function loadApp() {
+	
+	var flipbook = $('.sj-book');
+
+	// Check if the CSS was already loaded
+	
+	if (flipbook.width()==0 || flipbook.height()==0) {
+		setTimeout(loadApp, 10);
+		return;
+	}
+
+	// URIs	
+	Hash.on('^page\/([0-9]*)$', {
+		yep: function(path, parts) {
+
+			var page = parts[1];
+
+			if (page!==undefined) {
+				if ($('.sj-book').turn('is'))
+					$('.sj-book').turn('page', page);
+			}
+
+		},
+		nop: function(path) {
+
+			if ($('.sj-book').turn('is'))
+				$('.sj-book').turn('page', 1);
+		}
+	});
+
+	// Arrows
+	if (window.ARROW_KEYS) {
+		$(document).keydown(function(e){
+
+			var previous = 37, next = 39;
+
+			switch (e.keyCode) {
+				case previous:
+
+					$('.sj-book').turn('previous');
+
+				break;
+				case next:
+					
+					$('.sj-book').turn('next');
+
+				break;
+			}
+
+		});
+	}
+
+	// Flipbook
+	flipbook.turn({
+		elevation: 50,
+		acceleration: !isChrome(),
+		autoCenter: true,
+		gradients: true,
+		duration: 1000,
+		pages: 112,
+		when: {
+			turning: function(e, page, view) {
+				
+				var book = $(this),
+					currentPage = book.turn('page'),
+					pages = book.turn('pages');
+
+				if (currentPage>3 && currentPage<pages-3) {
+				
+					if (page==1) {
+						book.turn('page', 2).turn('stop').turn('page', page);
+						e.preventDefault();
+						return;
+					} else if (page==pages) {
+						book.turn('page', pages-1).turn('stop').turn('page', page);
+						e.preventDefault();
+						return;
+					}
+				} else if (page>3 && page<pages-3) {
+					if (currentPage==1) {
+						book.turn('page', 2).turn('stop').turn('page', page);
+						e.preventDefault();
+						return;
+					} else if (currentPage==pages) {
+						book.turn('page', pages-1).turn('stop').turn('page', page);
+						e.preventDefault();
+						return;
+					}
+				}
+
+				updateDepth(book, page);
+				
+				if (page>=2)
+					$('.sj-book .p2').addClass('fixed');
+				else
+					$('.sj-book .p2').removeClass('fixed');
+
+				if (page<book.turn('pages'))
+					$('.sj-book .p111').addClass('fixed');
+				else
+					$('.sj-book .p111').removeClass('fixed');
+
+				Hash.go('page/'+page).update();
+					
+			},
+
+			turned: function(e, page, view) {
+
+				var book = $(this);
+
+				if (page==2 || page==3) {
+					book.turn('peel', 'br');
+				}
+
+				updateDepth(book);
+				
+				book.turn('center');
+
+			},
+
+			end: function(e, pageObj) {
+			
+				var book = $(this);
+
+				updateDepth(book);
+
+			},
+
+			missing: function (e, pages) {
+
+				for (var i = 0; i < pages.length; i++) {
+					addPage(pages[i], $(this));
+				}
+
+			}
+		}
+	});
+
+	flipbook.addClass('animated');
+
+	// Show canvas
+
+	$('#canvas').css({visibility: ''});
+}
+
+function isChrome() {
+
+	// Chrome's unsolved bug
+	// http://code.google.com/p/chromium/issues/detail?id=128488
+
+	return navigator.userAgent.indexOf('Chrome')!=-1;
+
+}
---
+++ phonebook/js/phonebook.js
@@ -0,0 +1,161 @@
+
+function maybe(str, possibility) {
+	possibility = possibility || 0.3;
+	if (Math.random() < possibility) {
+		return str;
+	} else {
+		return '';
+	}
+}
+
+var NUM_LISTINGS = 8000;
+var listings = [];
+
+function populateListings() {
+	for (var i = 0; i < NUM_LISTINGS; i++) {
+		if (Math.random() < 0.7) {
+			listings.push({
+				class: 'listing', 
+				last: faker.name.lastName(),
+				first: faker.name.firstName(),
+				address: faker.address.streetAddress() + ' ' + maybe(faker.address.zipCode()),
+				tel: faker.phone.phoneNumberFormat().substr(4, 10)
+			});
+		} else {
+			listings.push({
+				last: faker.name.lastName(),
+				class: 'listing-indented', 
+				first: faker.name.firstName() + maybe(' & ' + faker.name.firstName()),
+				tel: faker.phone.phoneNumberFormat().substr(4, 10),
+				address: maybe(faker.address.streetAddress()),
+				indented: true
+			});
+		}
+	}
+
+	listings = listings.sort(function(l1, l2) {
+		return l1.last.localeCompare(l2.last);
+	});
+}
+
+populateListings();
+
+
+
+
+function updateDepth(book, newPage) {
+
+	var page = book.turn('page'),
+		pages = book.turn('pages'),
+		depthWidth = 16*Math.min(1, page*2/pages);
+
+		newPage = newPage || page;
+
+	if (newPage>3)
+		$('.sj-book .p2 .depth').css({
+			width: depthWidth,
+			left: 20 - depthWidth
+		});
+	else
+		$('.sj-book .p2 .depth').css({width: 0});
+
+		depthWidth = 16*Math.min(1, (pages-page)*2/pages);
+
+	if (newPage<pages-3)
+		$('.sj-book .p111 .depth').css({
+			width: depthWidth,
+			right: 20 - depthWidth
+		});
+	else
+		$('.sj-book .p111 .depth').css({width: 0});
+
+}
+
+function generateHTML(page) {
+	var result = $.Deferred();
+	var start = (8000 / 112) * page;
+	var pageListings = listings.slice(start, start + 162);
+
+	var i = 0;
+	var end;
+	while (!end) {
+		if (pageListings[pageListings.length - i] && pageListings[pageListings.length - i].last) {
+			end = pageListings[pageListings.length - i].last.substr(0, 3);
+		} else {
+			i++;
+		}
+	}
+
+	var starter;
+	i = 0;
+	while(!starter) {
+		if (pageListings[i] && pageListings[i].last) {
+			starter = pageListings[i];
+		} else {
+			i++;
+		}
+	}
+
+	var context = {
+		listings: pageListings,
+		pageInfo: {
+			number: page,
+			letter: starter.last.substr(0, 1),
+			range: starter.last.substr(0, 3) + '-' + end
+		}
+	};
+
+	var source   = $("#entry-template").html();
+	var template = Handlebars.compile(source);
+
+	var html = template(context);
+	setTimeout(function() {
+		result.resolve(html);
+	}, 0);
+
+	return result;
+
+}
+
+
+function loadPage(page) {
+
+	generateHTML(page).done(function(pageHtml) {
+		$('.sj-book .p' + page).html(pageHtml);
+	});
+
+}
+
+function addPage(page, book) {
+
+	var id, pages = book.turn('pages');
+
+	if (!book.turn('hasPage', page)) {
+
+		var element = $('<div />', {
+			class: 'own-size'
+		}).html('<div class="loader"></div>');
+
+		if (book.turn('addPage', element, page)) {
+			loadPage(page);
+		}
+
+	}
+}
+
+function numberOfViews(book) {
+	return book.turn('pages') / 2 + 1;
+}
+
+function getViewNumber(book, page) {
+	return parseInt((page || book.turn('page'))/2 + 1, 10);
+}
+
+function isChrome() {
+
+	// Chrome's unsolved bug
+	// http://code.google.com/p/chromium/issues/detail?id=128488
+
+	return navigator.userAgent.indexOf('Chrome')!=-1;
+
+}
